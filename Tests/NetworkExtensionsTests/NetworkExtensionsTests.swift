c17c089ecde6565e437818ca4dce3238c0f32873
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2021-04-22 17:10:57 +0200

    Fix tests, by using en0 instead of en9

--- Tests/NetworkExtensionsTests/NetworkExtensionsTests.swift
+++ Tests/NetworkExtensionsTests/NetworkExtensionsTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class NetworkExtensionsTests: XCTestCase {
     func testIPv6WithInterface() {
         // given
-        let input = "fe80::521e:2dff:fe3d:1d38%en9"
+        let input = "fe80::521e:2dff:fe3d:1d38%en0"
 
         // when
         guard let sut = IP(input),
@@ -15,8 +15,8 @@ final class NetworkExtensionsTests: XCTestCase {
 
         // then
         XCTAssertTrue(sut.isIPv6)
-        XCTAssertEqual(v6addr.interface?.name, "en9")
-        XCTAssertEqual(sut.ipString, "fe80::521e:2dff:fe3d:1d38%en9")
+        XCTAssertEqual(v6addr.interface?.name, "en0")
+        XCTAssertEqual(sut.ipString, "fe80::521e:2dff:fe3d:1d38%en0")
     }
 
     func testIpv4UrlString() {
@@ -36,7 +36,7 @@ final class NetworkExtensionsTests: XCTestCase {
 
     func testIpv6UrlString() {
         // given
-        let input = "fe80::521e:2dff:fe3d:1d38%en9"
+        let input = "fe80::521e:2dff:fe3d:1d38%en0"
 
         // when
         guard let sut = IP(input),
@@ -47,15 +47,15 @@ final class NetworkExtensionsTests: XCTestCase {
 
         // then
         XCTAssertTrue(sut.isIPv6)
-        XCTAssertEqual(sut.ipUrlString, "[fe80::521e:2dff:fe3d:1d38%en9]")
+        XCTAssertEqual(sut.ipUrlString, "[fe80::521e:2dff:fe3d:1d38%en0]")
     }
 
     func testPreferredAddress() {
         // given
-        let addresses = ["127.0.0.1", "fe80::521e:2dff:fe3d:1d38%en9", "2003:e1:d716:4d00:54f9:61b5:2507:11bd", "1.1.1.1"]
+        let addresses = ["127.0.0.1", "fe80::521e:2dff:fe3d:1d38%en0", "2003:e1:d716:4d00:54f9:61b5:2507:11bd", "1.1.1.1"]
             .compactMap { IP($0) }
 
         // then
-        XCTAssertEqual(addresses.preferredAddress?.ipString, "fe80::521e:2dff:fe3d:1d38%en9")
+        XCTAssertEqual(addresses.preferredAddress?.ipString, "fe80::521e:2dff:fe3d:1d38%en0")
     }
 }
