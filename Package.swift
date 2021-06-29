ceaca521bb7b547ecab537e190db5a67ed3c50e8
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2021-06-29 17:32:38 +0200

    Add SPM Product AllStatic

--- Package.swift
+++ Package.swift
@@ -4,16 +4,21 @@ import PackageDescription
 let package = Package(
     name: "NetworkExtensions",
     platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
-    products: [.library(name: "NetworkExtensions", targets: ["NetworkExtensions"])],
+    products: [
+        .library(name: "NetworkExtensions", targets: ["NetworkExtensions"]),
+        .library(name: "NetworkExtensionsAllStatic", targets: ["NetworkExtensions"])
+    ],
     dependencies: [
-        .package(url: "https://github.com/teufelaudio/FoundationExtensions.git", .upToNextMajor(from: "0.1.5"))
+        .package(url: "https://github.com/teufelaudio/FoundationExtensions.git", .upToNextMajor(from: "0.1.7"))
     ],
     targets: [
         .target(
             name: "NetworkExtensions",
-            dependencies: [
-                .product(name: "FoundationExtensions", package: "FoundationExtensions")
-            ]
+            dependencies: [.product(name: "FoundationExtensions", package: "FoundationExtensions")]
+        ),
+        .target(
+            name: "NetworkExtensionsAllStatic",
+            dependencies: [.product(name: "FoundationExtensionsStatic", package: "FoundationExtensions")]
         ),
         .testTarget(name: "NetworkExtensionsTests", dependencies: ["NetworkExtensions"])
     ]
